356fa55c9efb08042a5c89ce555d47392fb8d95d
deepjaswalMNT <deep@masterinfotech.com>
2025-08-08 00:08:53 +0530

    minnner fixes

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -22,6 +22,6 @@ export const experiences = [
     id: 4,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Jan 2020 - Present)"
   }
 ]
